c79f816db168a99a8b944487b22ed433d7ba1da1
monika4701 <59799369+monika4701@users.noreply.github.com>
2020-03-28 17:00:10 +0530

    Update MyCircularQueue.java
    Creating a constructor, initialisation of the variable, creating an object.

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,40 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+    Node front;
+    Node rear;
+    int count;
+
+    public MyCircularQueue(){
+        front = null;
+        rear = null;
+        count = 0;
+    }
+
+    public void enqueue(int roll,int backlog,int a_count){
+        Student st = new Student(roll,backlog,a_count);
+        Node ns = new Node();
+        ns.setStudent(st);
+
+        if(front==null){
+            front = ns;
+        }
+        else{
+            rear.setNext(ns);
+        }
+
+        rear = ns;
+        rear.setNext(front);
+        count++;
+    }
+
+    
+
+
 }
