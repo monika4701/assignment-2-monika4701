1d9cecdd215a8afab1ff5fb53faccb22d5400f7e
monika4701 <59799369+monika4701@users.noreply.github.com>
2020-03-28 15:20:18 +0530

    Update MyBinarySearchTree.java
    Implementation of method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -14,5 +14,38 @@ public MyBinarySearchTree(){
        root=null;
        count=0;
 }
+public void insert(int data){
+        TreeNode ns = new TreeNode();
+        ns.setdata(data);
+        if(root == null){
+            root = ns;
+        }
+        else{
+            TreeNode current = root;
+            while(true){
+                if(data > current.getdata()){
+                    if(current.getRight()==null){
+                        current.setRight(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getRight();
+                    }
+                }
+                else{
+                    if(current.getLeft()==null){
+                        current.setLeft(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getLeft();
+                    }
+                }
+            }
+        }
+    }
+    
+
+    
 
 }
