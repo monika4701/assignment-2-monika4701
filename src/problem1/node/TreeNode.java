b48ded37ee5ac173211e8bd9efd8d8507a4b50e0
monika4701 <59799369+monika4701@users.noreply.github.com>
2020-03-28 11:11:22 +0530

    Update TreeNode.java
    Creating method

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,5 +11,30 @@ public class TreeNode {
     private TreeNode left;
     private TreeNode right;
 
+    public void setdata(int data){
+        this.data = data;
+    }
+
+    public int getdata(){
+        return data;
+    }
+
+    public void setRight(TreeNode right){
+        this.right = right;
+    }
+
+    public void setLeft(TreeNode left){
+        this.left = left;
+    }
+
+    public TreeNode getLeft(){
+        return left;
+    }
+
+    public TreeNode getRight(){
+        return right;
+    }
+
+
 
 }
